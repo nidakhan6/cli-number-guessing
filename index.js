aabd9199b36f13f0bb1cfdddddcb5ef6c1d0028d
NidaWorld <160415456+nidakhan6@users.noreply.github.com>
2024-03-24 20:13:54 +0500

    created a CLI Number Guessing Game

---
+++ index.js
@@ -0,0 +1,23 @@
+#! /usr/bin/env node 
+//#shabang
+import inquirer from "inquirer";
+console.log("\n\twellcome to codewithnida - CLI Number Guessing Game\n");
+// 1 computer will generate a random number 
+// 2 user input for guessing number 
+// 3 compare  user input with computer generated number and show result
+const randomNumber = Math.floor(Math.random() * 6 + 1);
+console.log("Welcome to number Guesing Game");
+const answers = await inquirer.prompt([
+    {
+        name: "userGuessedNumber",
+        type: "number",
+        message: " Please guess a number between 1 - 10:",
+    },
+]);
+///if (agar condition true hai )
+if (answers.userGuessedNumber === randomNumber) {
+    console.log("Congratulations! you guessed right number.");
+}
+else {
+    console.log("you guessed wrong number");
+}
